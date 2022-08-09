eff4ca5ef7e669d1d2999718fcde83a5e315a5c4
Daniel Kolesa <daniel@octaforge.org>
2022-08-09 22:31:05 +0200

    initial commit for libedit 20220411

---
+++ config.h
@@ -0,0 +1,31 @@
+/* customized for musl setups, tweak for others */
+
+/* pick one */
+#define HAVE_CURSES_H 1
+/* #undef HAVE_NCURSES_H */
+
+/* pick one */
+/* #undef HAVE_GETPW_R_DRAFT */
+#define HAVE_GETPW_R_POSIX 1
+
+#define HAVE_SYS_CDEFS_H 1
+#define HAVE_TERMCAP_H 1
+#define HAVE_TERM_H 1
+
+#define HAVE_GETLINE 1
+#define HAVE_ISSETUGID 1
+#define HAVE_STRLCAT 1
+#define HAVE_STRLCPY 1
+/* #undef HAVE_STRUCT_DIRENT_D_NAMLEN */
+/* #undef HAVE_STRUNVIS */
+/* #undef HAVE_STRVIS */
+#define HAVE_U_INT32_T 1
+/* #undef HAVE_VIS */
+
+#ifndef _GNU_SOURCE
+# define _GNU_SOURCE 1
+#endif
+
+#define HAVE_SIZE_MAX 1
+
+#include "sys.h"
