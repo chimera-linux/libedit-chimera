b9713616bd144a85f62d4358cd7c60905674bfa4
christos <christos@NetBSD.org>
2023-06-03 03:58:11 +0200

    Remove unused stuff, and limit the scope of some of the used ones. (from des@freebsd)

--- config.h
+++ config.h
@@ -12,20 +12,11 @@
 #define HAVE_TERMCAP_H 1
 #define HAVE_TERM_H 1
 
-#define HAVE_GETLINE 1
 #define HAVE_ISSETUGID 1
-#define HAVE_STRLCAT 1
-#define HAVE_STRLCPY 1
 /* #undef HAVE_STRUCT_DIRENT_D_NAMLEN */
-/* #undef HAVE_STRUNVIS */
-/* #undef HAVE_STRVIS */
-#define HAVE_U_INT32_T 1
-/* #undef HAVE_VIS */
 
 #ifndef _GNU_SOURCE
 # define _GNU_SOURCE 1
 #endif
-
-#define HAVE_SIZE_MAX 1
 
 #include "sys.h"
